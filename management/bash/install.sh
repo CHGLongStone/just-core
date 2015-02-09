a911683d24998b72972b396003e8592d8d29e256
CHGLongStone <jason.medland@gmail.com>
2015-02-09 01:02:27 -0500

    stub install script

--- management/bash/install.sh
+++ management/bash/install.sh
@@ -25,3 +25,47 @@ function usage(){
 	
 	"
 }
+
+#echo -e ${BLUE}"$0 $#args[$@] OPTIND[$OPTIND] OPTARG[$OPTARG]"${NC}
+while getopts ":v:t:h" name; do
+	echo -e "		${purple}FLAG:" $name "VALUE: $OPTARG${NC}"
+	#execution_string=$execution_string" -"$name" $OPTARG"
+	case $name in
+		d)  dflag=1
+			dval="$OPTARG";;
+		h)   usage 
+		exit 2;;
+	esac
+done
+
+#######################################
+#validate the input
+#######################################
+if [ ! -z "$dflag" ]; then
+	echo -e "${green}Creating Directory  ${NC}'"$dval"'"
+	mkdir 755 $dval
+	cp $DIR../../APIS/stub_AJAX  $dval/API
+	cp $DIR../../CACHE  $dval/CACHE
+	cp $DIR../../CONFIG  $dval/CONFIG
+	cp $DIR../../PACKAGES  $dval/PACKAGES
+	cp $DIR../../PLUGINS  $dval/PLUGINS
+	cp $DIR../../TEMPLATES  $dval/TEMPLATES
+	echo " 
+<?php
+
+$ENVPATH = '$DIR';
+$CACHEPATH = $dval'/CACHE';
+
+$PLUGINSPATH = $dval'/PLUGINS';
+$PACKAGESPATH = 'PACKAGES';
+$LOGPATH = '/var/log/apache2/';
+?>	
+	
+	" > $dval/env.php
+	
+else
+	echo -e "${red}NO DIR SET${NC}" 
+	echo echo "DIR NAME dval='" $dval "'"
+	usage
+	exit 2
+fi
