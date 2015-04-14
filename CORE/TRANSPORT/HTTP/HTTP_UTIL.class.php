0688a5c4b120bde2fc8b485b98516cf151bbdd38
Jason Medland <jason@turntwotech.com>
2015-04-14 17:08:22 -0400

    people who use implied closures make me want to punch them in the throat

--- CORE/TRANSPORT/HTTP/HTTP_UTIL.class.php
+++ CORE/TRANSPORT/HTTP/HTTP_UTIL.class.php
@@ -47,22 +47,28 @@ class HTTP_UTIL {
 			if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false) {
 				$iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
 				foreach ($iplist as $ip) {
-					if (\JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($ip))
+					if (\JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($ip)){
 						return $ip;
+					}
 				}
 			} else {
-				if (\JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
+				if (\JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_X_FORWARDED_FOR'])){
 					return $_SERVER['HTTP_X_FORWARDED_FOR'];
+				}
 			}
 		}
-		if (!empty($_SERVER['HTTP_X_FORWARDED']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_X_FORWARDED']))
+		if (!empty($_SERVER['HTTP_X_FORWARDED']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_X_FORWARDED'])){
 			return $_SERVER['HTTP_X_FORWARDED'];
-		if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+		}
+		if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])){
 			return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-		if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
+		}
+		if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_FORWARDED_FOR'])){
 			return $_SERVER['HTTP_FORWARDED_FOR'];
-		if (!empty($_SERVER['HTTP_FORWARDED']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_FORWARDED']))
+		}
+		if (!empty($_SERVER['HTTP_FORWARDED']) && \JCORE\TRANSPORT\HTTP\HTTP_UTIL::validate_ip($_SERVER['HTTP_FORWARDED'])){
 			return $_SERVER['HTTP_FORWARDED'];
+		}
 
 		// return unreliable ip since all else failed
 		return $_SERVER['REMOTE_ADDR'];
@@ -73,9 +79,9 @@ class HTTP_UTIL {
 	 * a private network range.
 	 */
 	public static function validate_ip($ip) {
-		if (strtolower($ip) === 'unknown')
+		if (strtolower($ip) === 'unknown'){
 			return false;
-
+		}
 		// generate ipv4 network address
 		$ip = ip2long($ip);
 
@@ -86,14 +92,30 @@ class HTTP_UTIL {
 			// signed numbers (ints default to signed in PHP)
 			$ip = sprintf('%u', $ip);
 			// do private network range checking
-			if ($ip >= 0 && $ip <= 50331647) return false;
-			if ($ip >= 167772160 && $ip <= 184549375) return false;
-			if ($ip >= 2130706432 && $ip <= 2147483647) return false;
-			if ($ip >= 2851995648 && $ip <= 2852061183) return false;
-			if ($ip >= 2886729728 && $ip <= 2887778303) return false;
-			if ($ip >;= 3221225984 && $ip <= 3221226239) return false;
-			if ($ip >;= 3232235520 && $ip <= 3232301055) return false;
-			if ($ip >= 4294967040) return false;
+			if ($ip >= 0 && $ip <= 50331647){ 
+				return false;
+			}
+			if ($ip >= 167772160 && $ip <= 184549375){ 
+				return false;
+			}
+			if ($ip >= 2130706432 && $ip <= 2147483647){ 
+				return false;
+			}
+			if ($ip >= 2851995648 && $ip <= 2852061183){ 
+				return false;
+			}
+			if ($ip >= 2886729728 && $ip <= 2887778303){ 
+				return false;
+			}
+			if ($ip >= 3221225984 && $ip <= 3221226239){ 
+				return false;
+			}
+			if ($ip >= 3232235520 && $ip <= 3232301055){ 
+				return false;
+			}
+			if ($ip >= 4294967040){ 
+				return false;
+			}
 		}
 		return true;
 	}
